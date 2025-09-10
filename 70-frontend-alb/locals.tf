80f2630291d90a9784b811e6d6a61d3b3d3d4fdb
sriharibandi <shreharii@gmail.com>
2025-09-10 22:09:29 +0530

    terraform-aws-eks

--- 70-frontend-alb/locals.tf
+++ 70-frontend-alb/locals.tf
@@ -1,7 +1,7 @@
 locals {
   vpc_id = data.aws_ssm_parameter.vpc_id.value
   public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
-  frontend_alb_sg_id = data.aws_ssm_parameter.frontend_alb_sg_id.value
+  ingress_alb_sg_id = data.aws_ssm_parameter.ingress_alb_sg_id.value
   acm_certificate_arn = data.aws_ssm_parameter.acm_certificate_arn.value
   common_tags = {
     Project = var.project
--- 70-frontend-alb/parameters.tf
+++ 70-frontend-alb/parameters.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "ingress_alb_listner_arn" {
   name  = "/${var.project}/${var.environment}/ingress_alb_listner_arn"
   type  = "String"
-  value = aws_lb_listener.frontend_alb.arn
+  value = aws_lb_listener.ingress_alb.arn
   overwrite = true
 }
