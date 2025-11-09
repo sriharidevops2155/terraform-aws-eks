b6f4124e49e11d4bda76344b70dbe6e07c32ef11
sriharibandi <shreharii@gmail.com>
2025-11-10 00:29:09 +0530

    catalogue-cd

--- 80-eks/provider.tf
+++ 80-eks/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "6.15.0"
+      version = "6.0.0"
     }
   }
    backend "s3" {  #Dont save the state file in local and save the state file in below S3 bucket 
